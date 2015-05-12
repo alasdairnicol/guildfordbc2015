b6d03b77bfd20f5849568e9f1cb608fe6cb6f91d
Alasdair Nicol <alasdair@thenicols.net>
2015-05-12 01:03:44 +0100

    Added method to put all wards in a single json file
     * use proper variable names instead of e.g. 'w'

--- parse_wards.py
+++ parse_wards.py
@@ -7,29 +7,43 @@ import requests
 WARD_TYPE = "District council ward"
 CURRENT_GENERATION = 23
 
-def get_wards():
+def get_ward_info():
     """
     Construct a dictionary of wards
     """
     with open("children.json") as f:
         children = json.loads(f.read())
-    wards = {
+    ward_info = {
         k:v for k,v in children.items() if v['type_name'] == WARD_TYPE and v['generation_high'] == CURRENT_GENERATION
     }
-    return wards
+    return ward_info
 
-def fetch_boundaries(wards):
+def fetch_boundaries(ward_info):
     """
     Fetch the ward boundaries from the mapit site
     """
-    for w in wards.values():
-        print("Fetching ward {}".format(w['name']))
-        url = "http://mapit.mysociety.org/area/{}.geojson".format(w['id'])
+    for ward in ward_info.values():
+        print("Fetching ward {}".format(ward['name']))
+        url = "http://mapit.mysociety.org/area/{}.geojson".format(ward['id'])
         response = requests.get(url)
-        filename = "wards/{}.geojson".format(w['id'])
+        filename = "wards/{}.geojson".format(ward['id'])
         with open(filename, 'w') as f:
             f.write(response.content.decode('utf-8'))
 
+def construct_borough_json(ward_info):
+    """
+    Creates a single json file with all the wards
+    """
+    shapes = {}
+    for ward_id, ward in ward_info.items():
+        filename = "wards/{}.geojson".format(ward['id'])
+        with open(filename, 'r') as f:
+            shapes[ward_id] = json.loads(f.read())
+
+    with open("wards/guildford.json", 'w') as f:
+        f.write(json.dumps(list(shapes.values())))
+
 if __name__ == "__main__":
-    wards = get_wards()
-    fetch_boundaries(wards)
+    ward_info = get_ward_info()
+    # fetch_boundaries(ward_info)
+    construct_borough_json(ward_info)

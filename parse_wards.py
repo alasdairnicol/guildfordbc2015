865310927ed70a75b055fc87286ee154acb4577b
Alasdair Nicol <alasdair@thenicols.net>
2015-10-17 23:14:03 +0100

    Updated script to handle county boundaries

--- parse_wards.py
+++ parse_wards.py
@@ -4,21 +4,39 @@ import json
 
 import requests
 
-WARD_TYPE = "District council ward"
-CURRENT_GENERATION = 23
+CURRENT_GENERATION = 25
 
-def get_ward_info():
+GUILDFORD_BOROUGH_COUNCIL = {
+    'id': 2452,
+    'type': 'DIS',
+    'ward_type': 'DIW',
+    'name': 'Guildford Borough Council',
+}
+
+SURREY_COUNTY_COUNCIL = {
+    'id': 2242,
+    'type': 'CTY',
+    'ward_type': 'CED',
+    'name': 'Surrey County Council'
+}
+
+def get_ward_info(council):
     """
     Construct a dictionary of wards
     """
-    with open("children.json") as f:
-        children = json.loads(f.read())
+    url = "http://mapit.mysociety.org/area/{}/children".format(council['id'])
+    response = requests.get(url)
+    
+    with open("{}/children.json".format(council['name']), 'wb') as f:
+        f.write(response.content)
+    
+    children = response.json()
     ward_info = {
-        k:v for k,v in children.items() if v['type_name'] == WARD_TYPE and v['generation_high'] == CURRENT_GENERATION
+        k:v for k,v in children.items() if v['type'] == council['ward_type'] and v['generation_high'] == CURRENT_GENERATION
     }
     return ward_info
 
-def fetch_boundaries(ward_info):
+def fetch_boundaries(council, ward_info):
     """
     Fetch the ward boundaries from the mapit site
     """
@@ -26,11 +44,11 @@ def fetch_boundaries(ward_info):
         print("Fetching ward {}".format(ward['name']))
         url = "http://mapit.mysociety.org/area/{}.geojson".format(ward['id'])
         response = requests.get(url)
-        filename = "wards/{}.geojson".format(ward['id'])
-        with open(filename, 'w') as f:
-            f.write(response.content.decode('utf-8'))
+        filename = "{}/wards/{}.geojson".format(council['name'], ward['id'])
+        with open(filename, 'wb') as f:
+            f.write(response.content)
 
-def construct_borough_json(ward_info):
+def construct_borough_json(council, ward_info):
     """
     Creates a single json file with all the wards
     """
@@ -40,16 +58,17 @@ def construct_borough_json(ward_info):
         ]
     }
     for ward_id, ward in ward_info.items():
-        filename = "wards/{}.geojson".format(ward['id'])
+        filename = "{}/wards/{}.geojson".format(council['name'], ward['id'])
         with open(filename, 'r') as f:
             polygon = json.loads(f.read())
             polygon['properties'] = {'name': ward['name']}
             collection['features'].append(polygon)
 
-    with open("wards/guildford.geojson", 'w') as f:
+    with open("{}/wards/{}.geojson".format(council['name'], council['name']), 'w') as f:
         f.write(json.dumps(collection))
 
 if __name__ == "__main__":
-    ward_info = get_ward_info()
-    # fetch_boundaries(ward_info)
-    construct_borough_json(ward_info)
+    council = SURREY_COUNTY_COUNCIL
+    ward_info = get_ward_info(council)
+    # fetch_boundaries(council, ward_info)
+    construct_borough_json(council, ward_info)
